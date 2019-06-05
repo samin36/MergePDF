63635a8f49fc0bf1e5d10dfdec59bae220ad6cc4
Shrey Amin <samin36@gatech.edu>
2019-06-05 16:43:04 -0400

    Readme updated with usage instructions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Python script that merges PDF files
+
+How to use:
+
+1) Navigate to the directory with the pdf files
+2) Run merger.py
+
+OR
+
+1) Provide the complete path of the folder with pdf files as a command line argument when running merger.py:
+    Ex: python merger.py --path "THE PATH"
+        (Make sure to leave out a backslash at the end when entering the path)
+        Example -> "C:\Users\Shrey" NOT "C:\Users\Shrey\"
--- merger.py
+++ merger.py
@@ -2,13 +2,23 @@
 from PyPDF2 import PdfFileMerger
 import tqdm
 import os
+import argparse
 
-path = os.getcwd() + "\\"
-pdf_files = [file for file in os.listdir(path) if os.path.isfile(file) and file.endswith('.pdf') ]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, help="complete path of the folder with pdf files")
+args = parser.parse_args()
+
+path = args.path if args.path else os.getcwd()
+
+path += "\\"
+
+pdf_files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file)) and file.endswith('.pdf') ]
 
 if pdf_files:
     merger = PdfFileMerger()
     for files in pdf_files:
+        print(f"Adding {files}...")
         merger.append(path + files)
 
     while True:
@@ -27,4 +37,4 @@ if pdf_files:
 
     merger.close()
 else:
-    print(f"No PDFS in the current directory: {os.getcwd()}")
+    print(f"No PDFS in the directory: {path}")
